98608e81f1411e4997a314a55b1f1e9a38c91af2
Maxwell Boucher <mboucher18@nait.ca>
2023-02-15 14:39:31 -0700

    dice roll

---
+++ RazprPagesDemo/RazprPagesDemo/Pages/RollDice.cshtml.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazprPagesDemo.Pages
+{
+    public class RollDiceModel : PageModel
+    {
+        public void OnGet()
+        {
+           //DieImage = "/img/icons/000000/ffffff/1x1/delapouite/perspective-dice-six-faces-three.png";
+        }
+        public int DieValue { get; private set; }
+        public string DieImage { get; private set; } = "/img/icons/000000/ffffff/1x1/delapouite/perspective-dice-six-faces-three.png";
+
+        public string[] DieImages =
+        {
+            "/img/icons/000000/ffffff/1x1/delapouite/dice-six-faces-one.png",
+            "/img/icons/000000/ffffff/1x1/delapouite/dice-six-faces-two.png",
+            "/img/icons/000000/ffffff/1x1/delapouite/dice-six-faces-three.png",
+            "/img/icons/000000/ffffff/1x1/delapouite/dice-six-faces-four.png",
+            "/img/icons/000000/ffffff/1x1/delapouite/dice-six-faces-five.png",
+            "/img/icons/000000/ffffff/1x1/delapouite/dice-six-faces-six.png"
+        };
+        public void OnPost()
+        {
+            var ran = new Random();
+            DieValue = ran.Next(1, 7);
+            DieImage = DieImages[DieValue - 1];
+        }
+    }
+}
